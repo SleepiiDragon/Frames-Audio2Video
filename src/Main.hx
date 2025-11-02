340e0bce785949080c1945b237baa35b2efdf352
SleepiiDragon <altac-lancing@hotmail.com>
2025-11-02 14:11:25 +1000

    everything else

---
+++ src/Main.hx
@@ -0,0 +1,91 @@
+/**
+    toVideo.exe --INFOJSON!mrow.json
+**/
+
+import haxe.Json;
+import sys.FileSystem;
+import sys.io.File;
+
+class JsonType {
+    public var frameNames:Null<String>;
+    public var fps:Null<Int>;
+    public var audioName:Null<String>;
+    public var folder:Null<String>;
+    public var frame0sAmt:Null<Int>;
+    public var frameFormat:Null<String>;
+    public var outFile:Null<String>;
+}
+
+class Main{
+    static var argEnd:Map<String, String> = new Map<String, String>();
+
+
+    public static function checkAndYell(thing:Dynamic, name:String){
+        if(thing == null){
+            throw(name + " IS NULL!! it cannot be!!");
+        }
+    }
+    public static function main(){
+        var args = Sys.args();
+        for(arg in args){
+            //Sys.println(arg);
+            //var realArg = arg
+            var argSplitted = arg.split("!");
+            if(argSplitted.length > 1){
+                var argFirst = argSplitted[0];
+                argFirst = argFirst.substring(2, argFirst.length);
+                argSplitted.remove(argSplitted[0]);
+
+                trace(argFirst);
+                argEnd[argFirst] = argSplitted.join("!");
+            }
+        }
+
+        if(!argEnd.exists("INFOJSON")){
+            throw("no infojson!");
+        }
+
+        //Sys.command("cd", [argEnd.get("INFOJSON")]);
+        //trace(argEnd.get("INFOJSON"));
+        
+        var jsonpath = "./" + argEnd.get("INFOJSON");
+        if(FileSystem.exists(jsonpath)){
+            var theJson = File.getContent(jsonpath);
+            var cookedJson:JsonType = Json.parse(theJson);
+
+            var shouldDOTDOTout = false;
+            if(cookedJson.folder != null){
+                Sys.setCwd(cookedJson.folder);
+                shouldDOTDOTout = true;
+                trace(Sys.getCwd());
+            }
+
+            checkAndYell(cookedJson, "cookedJson");
+            checkAndYell(cookedJson.frameNames, "frameNames");
+            checkAndYell(cookedJson.fps, "fps");
+            checkAndYell(cookedJson.audioName, "audioName");
+            checkAndYell(cookedJson.frame0sAmt, "frame0sAmt");
+            checkAndYell(cookedJson.frameFormat, "frameFormat");
+            checkAndYell(cookedJson.folder, "folder");
+            checkAndYell(cookedJson.outFile, "outFile");
+
+            if(FileSystem.exists("./" + cookedJson.outFile)){
+                FileSystem.deleteFile("./" + cookedJson.outFile);
+            }
+            var finalFrameNameStuff:String = cookedJson.frameNames + "%0" + Std.string(cookedJson.frame0sAmt) + "d." + cookedJson.frameFormat;
+
+            Sys.command("ffmpeg", ["-framerate", Std.string(cookedJson.fps), "-i", finalFrameNameStuff, "-i", cookedJson.audioName, "-c:v", "libx264",  "-pix_fmt", "yuv420p", "-c:a", "aac", cookedJson.outFile]);
+            if(shouldDOTDOTout){
+                Sys.setCwd("../");
+            }
+        }else{
+            throw("json dont exist!");
+        }
+
+        if(args[0] == "HELP"){
+            Sys.println("TO BE DONE!");
+            return;
+        }
+
+    }
+}
